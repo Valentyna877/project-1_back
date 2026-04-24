468de58f361a05fec4750e7c8e294c3fa5cb364e
Uffaser <gerasimenkom405@gmail.com>
2026-04-24 21:15:00 +0300

    Add pregnancy info endpoints and update routes

--- src/routes/weeksRoutes.js
+++ src/routes/weeksRoutes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { getBabyState, getMomState } from "../controllers/weeksController.js";
+import {
+  getBabyState,
+  getMomState,
+  getPregnancyInfo,
+  getPregnancyInfoPublic,
+} from "../controllers/weeksController.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
 
-router.get('/baby/:week', authenticate, getBabyState);
-router.get('/mom/:week', authenticate, getMomState);
+router.get("/public", getPregnancyInfoPublic);
+router.get("/", authenticate, getPregnancyInfo);
+router.get("/baby/:week", authenticate, getBabyState);
+router.get("/mom/:week", authenticate, getMomState);
 
 export default router;
