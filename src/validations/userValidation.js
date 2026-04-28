66a4a545b50d43b3b5a74d7399d12327b922dfdc
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-04-29 00:17:47 +0300

    refactor: update gender validation to use constants

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,15 +1,16 @@
 import { Joi, Segments } from "celebrate";
 import { FORTY_WEEKS } from "../constants/times.js";
+import { GENDERS } from "../constants/genders.js";
 
 export const updateUserSchema = {
   [Segments.BODY]: Joi.object({
-    gender: Joi.string().valid("boy", "girl").allow(null).default(null),
+    gender: Joi.string().valid(GENDERS).allow(null).default(null),
     date: Joi.date().default(() => new Date(Date.now() + FORTY_WEEKS)),
   }),
 };
 
 export const updateUserGenderSchema = {
   [Segments.BODY]: Joi.object({
-    gender: Joi.string().valid("boy", "girl").allow(null).required(),
+    gender: Joi.string().valid(GENDERS).allow(null).required(),
   }),
 };
