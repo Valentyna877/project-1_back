27b4cb327922986953575519c079587dd4b374ce
Valentyna <valentine177@gmail.com>
2026-04-29 17:04:38 +0300

    Back fixes

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,10 +1,12 @@
 import { Joi, Segments } from "celebrate";
 import { FORTY_WEEKS } from "../constants/times.js";
-import { GENDERS } from "../constants/genders.js";
 
 export const updateUserSchema = {
   [Segments.BODY]: Joi.object({
-    gender: Joi.string().valid(GENDERS).allow(null).default(null),
+    gender: Joi.string()
+      .valid("boy", "girl", "unknown")
+      .allow(null)
+      .default(null),
     date: Joi.date().default(() => new Date(Date.now() + FORTY_WEEKS)),
     name: Joi.string().min(2).max(30).optional(),
     newEmail: Joi.string().email().optional().messages({
@@ -15,6 +17,6 @@ export const updateUserSchema = {
 
 export const updateUserGenderSchema = {
   [Segments.BODY]: Joi.object({
-    gender: Joi.string().valid(GENDERS).allow(null).required(),
+    gender: Joi.string().valid("boy", "girl", "unknown").allow(null).required(),
   }),
 };
